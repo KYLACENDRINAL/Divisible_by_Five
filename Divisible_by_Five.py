837bb91d1720807be31a3784dfe141d5c3474f35
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-20 22:41:27 +0800

    Done checking if the number in the list is divisible by 5 and printing numbers divisible by 5

--- Divisible_by_Five.py
+++ Divisible_by_Five.py
@@ -6,5 +6,9 @@ print ("Given list is [10, 20, 33, 46, 55]")
 print ("Divisible by 5")
 
 # Check if the number in the list is divisible by 5
+num_list=[10, 20, 33, 46, 55]
+for i in num_list:
+    if i%5==0:
 # Print numbers in the list divisible by 5
+        print (i)
  
